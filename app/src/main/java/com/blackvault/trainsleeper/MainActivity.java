f1f67a41f16a082abc14885288da149cd2ae853c
Peter Stuart <blackvault@users.noreply.github.com>
2017-05-22 19:09:53 +0100

    Added basic gesture control, swipe, double tap and long press.

--- app/src/main/java/com/blackvault/trainsleeper/MainActivity.java
+++ app/src/main/java/com/blackvault/trainsleeper/MainActivity.java
@@ -1,9 +1,7 @@
 package com.blackvault.trainsleeper;
 
 import android.Manifest;
-
 import android.content.Context;
-
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
@@ -13,20 +11,21 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.DecimalFormat;
-
 public class MainActivity extends AppCompatActivity implements LocationListener {
 
-    LocationManager mLocationManager;
-    String mLocationProvider;
+    private LocationManager mLocationManager;
+    private String mLocationProvider;
+    private GestureDetectorCompat mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +33,8 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mDetector = new GestureDetectorCompat(this, new MyGestureListener(getBaseContext()));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -65,6 +66,11 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -131,4 +137,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     public void onProviderDisabled(String provider) {
 
     }
+
+
 }
